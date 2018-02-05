58e2cc1cad694dfb6130a9a940c3f1951b1fd0c3
Chris Pelzer <chrispelzer@gmail.com>
2018-02-05 14:23:07 -0500

    add WSU custom plugins and set up the build-config with them

---
+++ plugins/buttonwayne/plugin.js
@@ -0,0 +1,13 @@
+﻿CKEDITOR.plugins.add( 'buttonwayne', {
+    icons: 'buttonwayne',
+    init: function( editor ) {
+        editor.addCommand( 'buttondialog', new CKEDITOR.dialogCommand( 'buttondialog' ) );
+        editor.ui.addButton( 'Buttonwayne', {
+            label: 'Insert Button',
+            command: 'buttondialog',
+            toolbar: 'buttonwayne'
+        });
+
+        CKEDITOR.dialog.add( 'buttondialog', this.path + 'dialogs/button.js' );
+    }
+});
