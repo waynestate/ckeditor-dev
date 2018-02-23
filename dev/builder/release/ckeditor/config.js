2f8271c77668a1bfc5ed7ea442340a72e745086c
Chris Pelzer <chrispelzer@gmail.com>
2018-02-23 16:34:32 -0500

    initial build with all our changes needed so far

---
+++ dev/builder/release/ckeditor/config.js
@@ -0,0 +1,57 @@
+﻿/**
+ * Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/terms-of-use/#open-source-licences
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+	config.skin = 'moono';
+	config.image2_altRequired = true;
+	config.pasteFromWordRemoveFontStyles = true;
+
+	// Remove allowed margins when pasting from Word
+	this.on('afterPasteFromWord', function(evt){
+		var filter = evt.editor.filter.clone(),
+			fragment = CKEDITOR.htmlParser.fragment.fromHtml( evt.data.dataValue ),
+			writer = new CKEDITOR.htmlParser.basicWriter();
+
+		// Disallow certain styles.
+		filter.disallow( 'p{margin-*}' );
+
+		// Process, and overwrite evt.data.dataValue.
+		filter.applyTo( fragment );
+		fragment.writeHtml( writer );
+		evt.data.dataValue = writer.getHtml();
+	});
+
+	config.toolbar_page = [
+		['Source','-','Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker', 'Scayt'],
+		['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+		['Image','Table','HorizontalRule','SpecialChar','PageBreak'],
+		'/',
+		['Bold','Italic','Strike','-','Subscript','Superscript'],
+		['NumberedList','BulletedList','-','Outdent','Indent','Blockquote','CreateDiv'],
+		['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+		['Link','Unlink','Anchor','Youtube'],
+		'/',
+		['Format','FontSize'],
+		['Maximize', 'ShowBlocks','-','About']
+	];
+	config.toolbar_email = [
+		['Source','-','Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker', 'Scayt'],
+		['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+		['Image','Table','HorizontalRule','SpecialChar','PageBreak'],
+		'/',
+		['Bold','Italic','Strike','-','Subscript','Superscript'],
+		['NumberedList','BulletedList','-'],
+		['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+		['Link','Unlink','Anchor','ButtonWayne','YoutubeThumb'],
+		'/',
+		['Format','FontSize'],
+		['Maximize', 'ShowBlocks','-','About']
+	];
+
+};
+
