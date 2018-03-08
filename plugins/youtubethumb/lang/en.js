e4dbd217e1387e3ce55b47520e6245c7269c2603
Chris Pelzer <chrispelzer@gmail.com>
2018-03-08 11:39:26 -0500

    fix youtubethumb dialog to not be applied to table plugin

--- plugins/youtubethumb/lang/en.js
+++ plugins/youtubethumb/lang/en.js
@@ -2,7 +2,7 @@
 Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang( 'table', 'en', {
+CKEDITOR.plugins.setLang( 'youtubethumb', 'en', {
 	border: 'Show frame border',
 	noUrl: 'Please type the youtube URL',
 	scrolling: 'Enable scrollbars',
